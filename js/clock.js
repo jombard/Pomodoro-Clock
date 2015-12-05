89fef40cdde2baedf607231d457820746ee8a5a8
Jon Lombard <jombard@hotmail.com>
2015-12-05 18:19:59 +0000

    Timer pause functionality

--- js/clock.js
+++ js/clock.js
@@ -7,7 +7,7 @@ $(function() {
 	});
 	$(".js-short-break").click(function() {
 		$("#timer_default").createTimer({
-			time_in_seconds: 300,
+			time_in_seconds: 5,
 			autostart: true
 		});
 	});
@@ -17,14 +17,35 @@ $(function() {
 			autostart: true
 		});
 	});
-	$("#timer_toggle").click(function() {
-		if ($("#timer_default").data('countdown.state') == 'running') {
-			$("#timer_default").pauseTimer();
-		} else {
-			$("#timer_default").startTimer();
-		}
-	});
-	$("#timer_reset").click(function() {
-		$("#timer_default").resetTimer();
-	});
+	$("#timer_toggle").click(timerToggle);
+	$("#timer_reset").click(resetTimer);
 });
+
+function countdownAlert() {
+	var sound = new Howl({
+		urls: ['sounds/alarmwatch.mp3']
+	}).play();
+}
+
+function timerToggle() {
+	var timer = $("#timer_default");
+	var timerToggle = $("#timer_toggle");
+
+	timerToggle.toggleClass('active');
+	if (timer.data('countdown.state') == 'running') {
+		timer.pauseTimer();
+		timerToggle.find('.glyphicon').removeClass('glyphicon-pause').addClass('glyphicon-play');
+		$('.pause-timer').text('Resume Timer');
+	} else {
+		timer.startTimer();
+		timerToggle.find('.glyphicon').removeClass('glyphicon-play').addClass('glyphicon-pause');
+		$('.pause-timer').text('Pause Timer');
+	}
+}
+
+function resetTimer() {
+	$("#timer_default").resetTimer();
+	$("#timer_toggle").find('.glyphicon').removeClass('glyphicon-play').addClass('glyphicon-pause');
+	$('.pause-timer').text('Pause Timer');
+	$("#timer_toggle").removeClass('active');
+}
